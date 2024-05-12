be233b0f98157a182672fa226ad9b77226bd9135
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-05-12 20:20:47 -0300

    feat: create tailwind color values

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['"Nunito"', "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#F15156",
+          900: "#E44449",
+          800: "#F75F64 ",
+          700: "#FC8686",
+        },
+        secondary: {
+          DEFAULT: "#0D3B66",
+          900: "#D3E2E5",
+          800: "#F5F8FA",
+        },
+        tertiary: {
+          DEFAULT: "#F4D35E",
+        },
+        green: {
+          DEFAULT: "#3CDC8C",
+        },
+      },
+    },
   },
   plugins: [],
 };
