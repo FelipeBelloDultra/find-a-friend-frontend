01a715601ea8269ea2a2e441b73873ea390f7550
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-05-18 23:25:13 -0300

    fix: change margin to padding

--- src/modules/shared/components/input/atoms/input-label.tsx
+++ src/modules/shared/components/input/atoms/input-label.tsx
@@ -7,7 +7,7 @@ interface InputLabelProps {
 export function InputLabel({ to, children }: PropsWithChildren<InputLabelProps>) {
   return (
     <label
-      className="w-full inline-block font-bold text-base mb-2 ml-1"
+      className="w-full inline-block font-bold text-base pb-2 ml-1"
       htmlFor={to}
       data-testid="input-label"
     >
